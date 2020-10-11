3cbc520b9162301c5cdc5dc716382512b1b5431e
noamlevi <lnoam2001@gmail.com>
2020-10-11 09:37:09 +0300

    add game component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App/App';
-import PoseNet from './Components/PoseNet/PoseNet';
+import Game from './Components/Game/Game';
 import {BrowserRouter as Router, Route} from "react-router-dom";
 
 
 const routing = (
     <Router>
         <Route exact path={"/"} component={App}/>
-        <Route exact path={"/game"} component={PoseNet}/>
+        <Route exact path={"/game"} component={Game}/>
     </Router>
 );
 
